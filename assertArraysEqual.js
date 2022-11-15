ef6c4fba39a9cc73699478be2828a0d8dbc7e3ed
Hilora Dewett <h.dewett@outlook.com>
2022-11-15 18:34:59 -0500

    fixed function name

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,4 +1,4 @@
-const eqArrays = function (arry1, arry2) {
+const eqArraysEqual = function (arry1, arry2) {
   if (arry1.length !== arry2.length) {
     console.log("Array lengths do not much. Arrays are not equal.");
     return false;
