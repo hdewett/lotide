d0faee6f2ebf5f39da1605ba08d058fba3fdac61
Hilora Dewett <h.dewett@outlook.com>
2022-11-15 01:43:43 -0500

    function similar to console.assert()

---
+++ assertEqual.js
@@ -0,0 +1,13 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log("🛑🛑🛑 Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log("✅✅✅ Assertion Failed: " + actual + " !== " + expected);
+  }
+};
+
+//TEST CODE
+console.log(assertEqual("Lighthouse Labs", "Bootcamp"));
+console.log(assertEqual(1, 1));
+console.log(assertEqual("Bootcamp", "Bootcamp"));
+console.log(assertEqual(1, 4));
