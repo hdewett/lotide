2d7ccd6d42a0354708187e2a928d8a0e444ee73a
Hilora Dewett <h.dewett@outlook.com>
2022-11-16 14:54:07 -0500

    included function description and updated test cases

--- eqArrays.js
+++ eqArrays.js
@@ -6,6 +6,9 @@ const assertEqual = function (actual, expected) {
   }
 };
 
+//ACTUAL FUNCTION
+//function eqArrays which takes in two arrays and returns true or false, based on a perfect match
+
 const eqArrays = function (arry1, arry2) {
   if (arry1.length !== arry2.length) {
     return false;
@@ -20,9 +23,9 @@ const eqArrays = function (arry1, arry2) {
 
 //TEST CASES
 
-eqArrays([1, 2, 3], [1, 2, 3]) // => true
-eqArrays([1, 2, 3], [3, 2, 1]) // => false
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true) // => true
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false) // => false
 
-eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
-eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true) // => true
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false) // => false
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
