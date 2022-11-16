a5a47c63aef3863e7b602032e73439a1b5b1cf18
Hilora Dewett <h.dewett@outlook.com>
2022-11-16 14:54:57 -0500

    updated function descriptions and updated assertArrayEqual to take input from eqArrays to console.log appropriate message

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,14 +1,23 @@
-const assertArraysEqual = function (arry1, arry2) {
+//function eqArrays which takes in two arrays and returns true or false, based on a perfect match
+const eqArrays = function (arry1, arry2) {
   if (arry1.length !== arry2.length) {
-    console.log("Array lengths do not much. Arrays are not equal.");
     return false;
   }
   for (let items in arry1) {
     if (arry1[items] !== arry2[items]) {
-      console.log("Arrays are not equal.");
       return false;
     }
   }
-  console.log("Arrays are equal.");
   return true;
+};
+
+//ACTUAL FUNCTION
+//this function compares two arrays and console.logs whether they match or not
+const assertArraysEqual = function (arry1, arry2) {
+  let result = eqArrays(arry1,arry2);
+  if (result===false) {
+    console.log("Arrays are not equal.");
+  } else if (result === true); {
+    console.log("Arrays are equal.");
+  }
 };
