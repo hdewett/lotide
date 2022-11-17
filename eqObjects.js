e78428c8ff0a1730016c59da4294a1ff12545e4e
Hilora Dewett <h.dewett@outlook.com>
2022-11-16 20:59:23 -0500

    incomplete function that returns true if objects have identical keys with identical values, otherwise false

---
+++ eqObjects.js
@@ -0,0 +1,52 @@
+//function compares two values and console.logs if they are the same or not
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+//function eqArrays which takes in two arrays and returns true or false, based on a perfect match
+const eqArrays = function (arry1, arry2) {
+  if (arry1.length !== arry2.length) {
+    return false;
+  }
+  for (let items in arry1) {
+    if (arry1[items] !== arry2[items]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+// ACTUAL FUNCTION
+// Returns true if both objects have identical keys with identical values.
+// Otherwise you get back a big fat false!
+const eqObjects = function(object1, object2) {
+  let object1Keys = Object.keys(object1);
+  let object2Keys = Object.keys(object2);
+  if (object1Keys.length !== object2Keys.length) {
+    return false;
+  }
+  for (let key of object1Keys) {
+    if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
+      if (!eqArrays(object1[key],object2[key])) {
+        return false;
+      }
+    } else if (object1[key] !== object2[key]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+
+
+// TEST CASES
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+assertEqual(eqObjects(cd, dc), true); // => true
+
+const cd2 = { c: "1", d: ["2", 3, 4] };
+assertEqual(eqObjects(cd, cd2), false); // => false
