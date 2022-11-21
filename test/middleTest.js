e48de10ee0bfed16d3b44f459ee2b01475f2b6e8
Hilora Dewett <h.dewett@outlook.com>
2022-11-21 18:31:35 -0500

    refactored middle.js and tail.js, and updated associated test files to use mocha and chai' '

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,16 +1,56 @@
 //test function
-const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 const middle = require('../middle');
 
-//Test cases for arrays with one or two elements
-assertArraysEqual(middle([1]), [])
-assertArraysEqual(middle([1, 2]), [])
+describe("#middle Test cases for arrays with one or two elements", () => {
+  let result1 = middle([1]);
+  let expected1 = [];
 
-//Test cases for arrays with odd number of elements 
-assertArraysEqual(middle([1, 2, 3]), [2])
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3])
+  let result2 = middle([1, 2]);
+  let expected2 = [];
 
-//Test cases for arrays with an even number of elements
+  it("returns '[]' for array with only one element", () => {
+    assert.deepEqual(result1, expected1);
+  });
 
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3])
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4])
+  it("returns '[]' for array with two element", () => {
+    assert.deepEqual(result2, expected2); 
+  });
+  
+});
+
+describe("#middle Test cases for arrays with odd number of elements", () => {
+  const result1 = middle([1, 2, 3]);
+  const expected1 = [2];
+
+  const result2 = middle([1, 2, 3, 4, 5]);
+  const expected2 = [3];
+  
+  it("returns '[2]' for '[1, 2, 3]'", () => {
+    assert.deepEqual(result1, expected1);
+  });
+
+  it("returns '[3]' for '[1, 2, 3, 4, 5]'", () => {
+    assert.deepEqual(result2, expected2); 
+  });
+
+});
+
+describe("#middle Test cases for arrays with an even number of elements", () => {
+  const result1 = middle([1, 2, 3, 4]);
+  const expected1 = [2, 3];
+
+  const result2 = middle([1, 2, 3, 4, 5, 6]);
+  const expected2 = [3, 4];
+  
+  it("returns '[2, 3]' for '[1, 2, 3, 4]'", () => {
+    assert.deepEqual(result1, expected1);
+  });
+
+  it("returns '[3, 4]' for '[1, 2, 3, 4, 5, 6]'", () => {
+    assert.deepEqual(result2, expected2); 
+  });
+
+});
+
+
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,22 +1,49 @@
-//Test Functions
-const assertEqual = require('../assertEqual');
-const tail = require('../tail');
+const assert = require('chai').assert;
+const tail   = require('../tail');
 
+describe("#tail Checking the returned array elements", () => {
+  const result = tail(["Hello", "Lighthouse", "Labs"]);
+  const expected = ["Lighthouse", "Labs"]
+  
+  it("returns '2' for result.length", () => {
+    assert.strictEqual(result.length, 2);
+  });
 
-//Test Case 1: Checking the returned array elements
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2);
-assertEqual(result[0], "Lighthouse");
-assertEqual(result[1], "Labs");
-//Test case 2: Making sure the original array is not modified
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3);
-//Test case 3: An array with only one element should yield an empty array for its tail
-const oneElement = [1];
-assertEqual(tail(oneElement).length, 0);
+  it("returns 'Lighthouse' for result[0]", () => {
+    assert.equal(result[0], "Lighthouse"); 
+  });
 
-//Test case 4: An empty array should yield an empty array for its tail
-const emptyList = [];
-tail(emptyList);
-assertEqual(emptyList.length, 0);
+  it("returns 'Labs' for result[1]", () => {
+    assert.deepEqual(result, expected); 
+  });
+
+});
+
+describe("#tail Making sure the original array is not modified", () => {
+  const words = ["Yo Yo", "Lighthouse", "Labs"];
+  tail(words);
+
+  it("returns '3' for words.length", () => {
+    assert.strictEqual(words.length, 3);
+  });
+
+});
+
+describe("#tail Checking that an array with only one element should yield an empty array for its tail", () => {
+  const oneElement = tail([1]);
+
+  it("returns '0' for oneElement.length", () => {
+    assert.strictEqual(oneElement.length, 0);
+  });
+
+});
+
+describe("#tail An empty array should yield an empty array for its tail", () => {
+  const emptyList = [];
+  tail(emptyList);
+
+  it("returns '0' for emptyList.length", () => {
+    assert.strictEqual(emptyList.length, 0);
+  });
+
+});
