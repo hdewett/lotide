e8741fca1a384fe198286ef9483a6957c94d7a79
Hilora Dewett <h.dewett@outlook.com>
2022-11-20 21:23:32 -0500

    updated comments to describe functions

--- head.js
+++ head.js
@@ -1,3 +1,4 @@
+//Testing functions
 const assertEqual = function (actual, expected) {
   if (actual === expected) {
     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
@@ -6,10 +7,13 @@ const assertEqual = function (actual, expected) {
   }
 };
 
+//ACTUAL FUNCTION
+//returns first item in an array
 const head = function (list) {
   return list[0];
 };
 
+//Test cases
 console.log(assertEqual(head([5, 6, 7]), 5));
 console.log(assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"));
 console.log(assertEqual(head([5]), 5));
